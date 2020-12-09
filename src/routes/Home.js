1a4c68da4d5856f9f5a838f1974523059fdb51ec
Jung <junghong91@naver.com>
2020-12-09 16:17:27 +0900

    #3.2 Getting the Tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
 import { dbService } from "fbase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const tweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [tweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getTweets();
+  }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("tweets").add({
@@ -31,6 +48,13 @@ const Home = () => {
         />
         <input type="submit" value="Upload" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <div key={tweet.id}>
+            <h4>{tweet.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
